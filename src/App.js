9b9ea14b70936f0c947253bbebccd8c61c9b76ca
malin7yt <MALIN7YT@GMAIL.COM>
2022-07-21 09:39:46 -0400

    Added form for Dictionary

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import logo from "./logo-2.png"
 import './App.css';
+import Dictionary from "./Dictionary";
 
 function App() {
   return (
     <div className="App">
+      <div className="container">
       <header className="App-header">
 <img src={logo} className="App-logo img-fluid" alt="logo" />
       </header>
+      <main>
+        <Dictionary />
+      </main>
+      <footer className="text-center App-footer">
+<small>Coded by Malin Jensen</small>
+      </footer>
+      </div>
     </div>
   );
 }
