f8155ca1e37d4c84a55ec98b587f8902fb39f254
malin7yt <MALIN7YT@GMAIL.COM>
2022-07-26 09:21:23 -0400

    Added CSS files and styles the application

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
 <img src={logo} className="App-logo img-fluid" alt="logo" />
       </header>
       <main>
-        <Dictionary />
+        <Dictionary defaultKeyword="sunset" />
       </main>
       <footer className="text-center App-footer">
 <small>
--- src/Results.js
+++ src/Results.js
@@ -7,6 +7,7 @@ export default function Results(props) {
     if (props.results) {
     return (
         <div className="Results">
+            <section>
             <h2>{props.results.word}</h2>
             {props.results.phonetics.map(function(phonetic, index) {
                 return (
@@ -16,11 +17,12 @@ export default function Results(props) {
                 );
 
             })}
+            </section>
             {props.results.meanings.map(function(meaning, index) {
 
-                return <div key={index}>
+                return <section key={index}>
                     <Meaning meaning={meaning} />
-                    </div>
+                    </section>
             })}
         </div>
         );
